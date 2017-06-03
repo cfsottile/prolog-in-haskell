d183d8d3a3e4368b78d07be04f2a791e9b17fd10
Cristian Sottile <cfsottile@gmail.com>
2017-06-03 17:59:53 -0300

    Fixed substitution lacking
    Now `unify` type is Term -> Term -> (Valuation,Substitution) -> Maybe (Valuation,Substitution).
    This allows to separate substitutions (var,var) from valuations (var,term). Every required
    substitution in order to make a term t and the head of a rule r) unify, is then applied to the
    goals of the rule. Thus, in any computational step of the proof search, the variables used are
    the ones from the goal.

--- prolog.hs
+++ prolog.hs
@@ -18,6 +18,7 @@ data Clause = Rule Term Goals deriving (Eq,Show,Ord) -- Rule's first argument mu
 type Program = [Clause]
 
 type Valuation = Map.Map Term Term -- Key must be Var
+type Substitution = Map.Map Term Term -- Key must be Var
 type ProgramMap = Map.Map Name [Clause]
 
 askIf :: Term -> Program -> Bool
@@ -29,16 +30,33 @@ askAll t p = ask t (genMap p) Map.empty
 -- | Takes a goal (PredTerm), a program (map) and a current valuation and
 -- collects all possible valuations that make it true. I.e., starts with a goal
 -- says all valuations that satisfy it.
+-- ask :: Term -> ProgramMap -> Valuation -> [Valuation]
+-- ask p@(Pred name _) pm val = 
+--     let possibleUnifiers = concat $ Map.lookup name pm
+--         unifyingValAndGoals clause = (unifyClause (Rule p []) clause val, goals clause)
+--         unifiers = (first fromJust <$>)       -- [(Valuation,Goals)]
+--                <$> filter (isJust . fst)      -- [(Maybe Valuation,Goals)]
+--                 $  unifyingValAndGoals        -- [(Maybe Valuation,Goals)]
+--                <$> possibleUnifiers           -- [Clause] -- Maybe [Clause]
+--         explore' (val',gs) = explore gs pm val'
+--     in concat $ explore' <$> unifiers
+
 ask :: Term -> ProgramMap -> Valuation -> [Valuation]
 ask p@(Pred name _) pm val = 
-    let possibleUnifiers = concat $ Map.lookup name pm
-        unifyingValAndGoals clause = (unifyClause (Rule p []) clause val, goals clause)
-        unifiers = (first fromJust <$>)       -- [(Valuation,Goals)]
-               <$> filter (isJust . fst)      -- [(Maybe Valuation,Goals)]
-                $  unifyingValAndGoals        -- [(Maybe Valuation,Goals)]
-               <$> possibleUnifiers           -- [Clause] -- Maybe [Clause]
+    let mayUnifyClauses = concat $ Map.lookup name pm
+        valuationAndGoals' = valuationAndGoals (Rule p []) val
         explore' (val',gs) = explore gs pm val'
-    in concat $ explore' <$> unifiers
+    in concat $ explore' <$> mapMaybe valuationAndGoals' mayUnifyClauses
+
+valuationAndGoals :: Clause -> Valuation -> Clause -> Maybe (Valuation,Goals)
+valuationAndGoals c1 val c2 = ((\sub -> substitute sub <$> goals c2) <$>) <$> unifyClause c1 c2 val
+    -- código viejo
+    -- case unifyClause c1 c2 val of
+    --     Just (val',sub) -> (val', substitute sub <$> goals c2)
+
+substitute :: Substitution -> Term -> Term
+substitute sub v@(Var _) = fromMaybe v (Map.lookup v sub)
+substitute sub (Pred name args) = Pred name (substitute sub <$> args)
 
 goals :: Clause -> Goals
 goals (Rule (Pred _ _) g) = g
@@ -73,32 +91,42 @@ getName :: Clause -> Maybe Name
 getName (Rule (Pred name _) _) = Just name
 getName _ = Nothing
 
-unifyClause :: Clause -> Clause -> Valuation -> Maybe Valuation
-unifyClause (Rule p1 _) (Rule p2 _) val = unify p1 p2 val
+unifyClause :: Clause -> Clause -> Valuation -> Maybe (Valuation,Substitution)
+unifyClause (Rule p1 _) (Rule p2 _) val = unify p1 p2 (val, Map.empty)
 
 -- | Takes two terms and a valuation. If terms unify, returns the compatible
 -- valuation that allows the unification. If not, returns Nothing.
-unify :: Term -> Term -> Valuation -> Maybe Valuation
-unify v@(Var _) t val = addVal v t val
-unify t v@(Var _) val = addVal v t val
+unify :: Term -> Term -> (Valuation,Substitution) -> Maybe (Valuation,Substitution)
+unify v1@(Var _) v2@(Var _) (val,sub) = addSub v1 v2 (val,sub)
+unify v@(Var _) t (val,sub) = addVal v t (val,sub)
+unify t v@(Var _) (val,sub) = addVal v t (val,sub)
 unify (Pred name1 args1) (Pred name2 args2) val
     | name1 == name2 && length args1 == length args2 = argsUnify args1 args2 val
     | otherwise = Nothing
 -- unify _ _ _ = Nothing
 
--- | Attempts to insert a variable instantiation in the Map. If the key was
--- present, checks if the associated value unify with the term ........ ???
-addVal :: Term -> Term -> Valuation -> Maybe Valuation
-addVal v@(Var _) term val = case Map.lookup v val of
-    Nothing -> Just $ Map.insert v term val
-    Just t -> unify t term val
+-- | Attempts to insert a variable substitution [v1/v2] in the Map. If v2 is
+-- present as a key, checks wether its value and v1 are the same and performs
+-- no changes. If not, the result is Nothing.
+addSub :: Term -> Term -> (Valuation,Substitution) -> Maybe (Valuation,Substitution)
+addSub v1@(Var name1) v2@(Var name2) (val,sub) = case Map.lookup v2 sub of
+    Nothing -> Just (val, Map.insert v2 v1 sub)
+    Just v' -> if v' == v1 then Just (val,sub) else Nothing
 
-argsUnify :: [Term] -> [Term] -> Valuation -> Maybe Valuation
-argsUnify [] [] val = Just val
+-- | Attempts to insert a variable instantiation v = term in the Map. If v is
+-- present as a key, the result is Maybe (Valuation,Substitution) from unifying
+-- v's value and term.
+addVal :: Term -> Term -> (Valuation,Substitution) -> Maybe (Valuation,Substitution)
+addVal v@(Var _) term (val,sub) = case Map.lookup v val of
+    Nothing -> Just (Map.insert v term val, sub)
+    Just t -> unify t term (val,sub) -- mmmmm
+
+argsUnify :: [Term] -> [Term] -> (Valuation,Substitution) -> Maybe (Valuation,Substitution)
+argsUnify [] [] (val,sub) = Just (val,sub)
 -- unify a1 a2 val :: Maybe Valuation
 -- argsUnify as1 as2 :: Valuation -> Maybe Valuation
 -- La mónada Maybe se encarga de continuar ante Just, y de cortar ante Nothing
-argsUnify (a1:as1) (a2:as2) val = unify a1 a2 val >>= argsUnify as1 as2
+argsUnify (a1:as1) (a2:as2) (val,sub) = unify a1 a2 (val,sub) >>= argsUnify as1 as2
     -- código anterior
     -- let u = unify a1 a2 val
     -- in case u of
